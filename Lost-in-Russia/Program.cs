033daa194482755b549c7ada8527cb6a622b82c4
DBanaszewski <dbanaszewski@onet.pl>
2018-07-15 22:00:43 +0200

    Small changes - 0.0.2.0

--- Lost-in-Russia/Program.cs
+++ Lost-in-Russia/Program.cs
@@ -46,17 +46,13 @@ namespace lir
         static void Main(string[] args)
         {
             Console.Title = "Lost in... RUSSIA";
-            Console.WriteLine(@" _               _      _             ");
-            Console.WriteLine(@"| |             | |    (_)            ");
-            Console.WriteLine(@"| |     ___  ___| |_    _ _ __        ");
-            Console.WriteLine(@"| |    / _ \/ __| __|  | | '_ \       ");
-            Console.WriteLine(@"| |___| (_) \__ \ |_   | | | | |_ _ _ ");
-            Console.WriteLine(@"|______\___/|___/\__|  |_|_| |_(_|_|_)");
+            Console.WriteLine("Ładowanie gry...");
             Thread.Sleep(1500);
             var file = new FileStream("test.ogg", FileMode.Open, FileAccess.Read);
             var player = new SoundPlayer(new OggDecodeStream(file));
             player.PlayLooping();
             Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
             Console.WriteLine(@" _               _      _                _____  _    _  _____ _____ _____          ");
             Console.WriteLine(@"| |             | |    (_)              |  __ \| |  | |/ ____/ ____|_   _|   /\    ");
             Console.WriteLine(@"| |     ___  ___| |_    _ _ __          | |__) | |  | | (___| (___   | |    /  \   ");
@@ -66,9 +62,37 @@ namespace lir
             Console.WriteLine("");
             Console.WriteLine("");
             Console.WriteLine("");
-            Print("Lost in... RUSSIA");
-            Print("Autor: DBanaszewski");
-            Print("Wersja: 0.0.1.0");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Lost in... RUSSIA");
+            Console.WriteLine("Autor: DBanaszewski & Paulina Jabłońska");
+            Console.WriteLine("Wersja: 0.0.2.0");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.CursorVisible = false;
+            Print("Kliknij dowolny przycisk, aby zacząć grę...");
+            Console.ReadKey();
+            Console.Clear();
+            player.Stop();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Print("3 marzec 1953 2:00 (wtorek)");
+            Thread.Sleep(1500);
+            Console.ForegroundColor = ConsoleColor.White;
+            Print("W środku nocy ktoś łomocze do drzwi.");
+            Thread.Sleep(2500);
+            Print("Otwierasz a tam smutni panowie w nie do końca dopasowanych mundurach.");
+            Thread.Sleep(2500);
+            Print("Już wiesz, że będzie źle.");
+            player.PlaySync();
+            Print("Już wiesz, że będzie źle.");
+            Print("Już wiesz, że będzie źle.");
+            Print("Już wiesz, że będzie źle.");
+            Print("Już wiesz, że będzie źle.");
+            Print("Już wiesz, że będzie źle.");
+            Print("Już wiesz, że będzie źle.");
+            Print("Już wiesz, że będzie źle.");
+            Print("Już wiesz, że będzie źle.");
+            Print("Już wiesz, że będzie źle.");
             Console.ReadKey();
         }
     }
